cc1a087c049be80274ff05e282e465bd11e9235d
Leneyxis <78430937+Leneyxis@users.noreply.github.com>
2023-05-14 14:26:24 +0530

    save progress

--- flashcard/main.py
+++ flashcard/main.py
@@ -5,28 +5,38 @@ from PIL import ImageTk
 from PIL import Image
 import pandas as pd
 import random as rand
-pos=0
+dic={}
 #----------------------data-setup----------------------------#
-data_frame=pd.read_csv(filepath_or_buffer="./data/french_words.csv")
-dic=pd.DataFrame.to_dict(self=data_frame, orient="records")
-french_words=data_frame["French"].tolist()
-english_words=data_frame["English"].tolist()
-word=rand.choice(french_words)
-pos=french_words.index(word)
+try:
+    data_frame=pd.read_csv("/data/Words_to_learn.csv")
+except:
+    original_data=pd.read_csv("./data/french_words.csv")
+    dic=original_data.to_dict(orient="records")
+else:
+    dic=data_frame.to_dict(orient="records")
+current_card=rand.choice(dic)
 def button_pressed():
-    global pos
+    global current_card
     window.after_cancel(change) # type: ignore
-    word=rand.choice(french_words)
-    pos=french_words.index(word)
+    current_card=rand.choice(dic)
     canvas.itemconfig(canvas_image,image=img)
     canvas.itemconfig(lang,text="French",fill="black")
-    canvas.itemconfig(clue,text=word,fill="black")
+    canvas.itemconfig(clue,text=current_card["French"],fill="black")
     window.after(3000,change)
 #----------------------flipcard----------------------------------#
+#----------------------Save Progress----------------------------#
+def correct():
+    dic.remove(current_card)
+    data=pd.DataFrame(dic)
+    data.to_csv("data/Words_to_learn.csv",index=False)
+    button_pressed()
+def incorrect():
+    button_pressed()
+#-------------------------------------------------------------------#
 def change():
     canvas.itemconfig(canvas_image, image=img2)
     canvas.itemconfig(lang,text="English",fill="white")
-    canvas.itemconfig(clue,text=f"{english_words[pos]}",fill="white")
+    canvas.itemconfig(clue,text=current_card["English"],fill="white")
 #----------------------UI Setup------------------------------#
 window=Tk()
 window.title("Flash Cards")
@@ -37,12 +47,12 @@ canvas_image=canvas.create_image(400,263,image=img)
 img2=PhotoImage(file="./images/card_back.png")
 canvas.grid(column=1,row=0,columnspan=2)
 lang=canvas.create_text(400,120,text="French",fill="black",font=("arial",30,"bold","italic"))
-clue=canvas.create_text(400,213,text=word,fill='black',font=("arial",40,'bold'))
+clue=canvas.create_text(400,213,text=current_card["French"],fill='black',font=("arial",40,'bold'))
 right=PhotoImage(file="./images/right.png")
 wrong=PhotoImage(file="./images/wrong.png")
-rightbutton=Button(image=right,padx=50,pady=50,highlightbackground=BACKGROUND_COLOR,highlightthickness=0,command=button_pressed)
+rightbutton=Button(image=right,padx=50,pady=50,highlightbackground=BACKGROUND_COLOR,highlightthickness=0,command=correct)
 rightbutton.grid(column=2,row=2)
-wrongbutton=Button(width=95,height=95,image=wrong,padx=50,pady=50,highlightthickness=0,command=button_pressed)
+wrongbutton=Button(width=95,height=95,image=wrong,padx=50,pady=50,highlightthickness=0,command=incorrect)
 wrongbutton.grid(column=1,row=2)
 window.after(3000,change)
 window.mainloop()
